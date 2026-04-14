dc4a6192d8aab6f625101000be5d02ddd7ad96c5
nitishjha18 <nitishjha1100@gmail.com>
2026-04-15 02:11:36 +0530

    Basic file structure setup

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log(sum);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 20;
+let sum = a + 9j j;
+
+console.log(sum);
